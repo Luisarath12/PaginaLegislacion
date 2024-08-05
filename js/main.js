d780b2e3c893ccd31f458d43c1011ca7f9309a10
AlexandherCM <alexandher_1321124129@uptecamac.edu.mx>
2024-08-04 23:22:30 -0600

    RedirectToRoute

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('.nav-link');
     const sections = document.querySelectorAll('.section');
     const navToggle = document.getElementById('nav-toggle');
@@ -8,21 +8,21 @@ document.addEventListener('DOMContentLoaded', function() {
         navMenu.classList.toggle('hidden');
     });
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const sectionId = this.getAttribute('data-section');
+//     navLinks.forEach(link => {
+//         link.addEventListener('click', function(event) {
+//             event.preventDefault();
+//             const sectionId = this.getAttribute('data-section');
 
-            sections.forEach(section => {
-                if (section.id === sectionId) {
-                    section.classList.remove('hidden');
-                } else {
-                    section.classList.add('hidden');
-                }
-            });
+//             sections.forEach(section => {
+//                 if (section.id === sectionId) {
+//                     section.classList.remove('hidden');
+//                 } else {
+//                     section.classList.add('hidden');
+//                 }
+//             });
 
-            // Hide the mobile menu after selecting a section
-            navMenu.classList.add('hidden');
-        });
-    });
-});
+//             // Hide the mobile menu after selecting a section
+//             navMenu.classList.add('hidden');
+//         });
+//     });
+// });
